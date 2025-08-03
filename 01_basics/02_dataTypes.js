f29b4986dfc368ec5c8e39329dc02b15efe12b0e
Atul Kumar <165009700+atulkumarkashyap@users.noreply.github.com>
2025-08-03 09:19:34 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) we are using node.js,not browser
+
+let name = "aman"
+let age = 18
+let isLoggedIn = false
+let state;
+let state1 = null
+
+// number
+// bigint
+// string = ""
+// boolean = true/false
+// null -> standalone value/empty value
+// undefined -> value not assigned
+// symbol -> unique 
+// object 
+console.log(typeof "aman")
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+
