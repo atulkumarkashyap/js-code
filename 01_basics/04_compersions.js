4918eb9e1a7d8a058839c915eeedf7653a8508ad
Atul Kumar <165009700+atulkumarkashyap@users.noreply.github.com>
2025-08-04 10:30:14 +0000

    conversions

---
+++ 01_basics/04_compersions.js
@@ -0,0 +1,19 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+// datatypes must be same while comparing two values
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// === strict check, it also check datatype
+
+console.log("2" === 2)
+
