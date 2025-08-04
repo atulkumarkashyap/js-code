8b68891be9f41c1fc23982b191edb7663b280c20
Atul Kumar <165009700+atulkumarkashyap@users.noreply.github.com>
2025-08-04 13:55:55 +0000

    datatypes1

---
+++ 01_basics/05_dataTypes-summery.js
@@ -0,0 +1,6 @@
+// how data is stored in memory and how data is access,base on this the data is divided into two
+// category primitive and non primitive
+// primitive datatypes are "call by value"
+// string,number,boolean,null,undefined,symbol,bigint
+// non primitive
+// array,objects,functions,
