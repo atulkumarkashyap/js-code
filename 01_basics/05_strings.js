3d6e0db75821ac99620137b2fef742d5d23288ce
Atul Kumar <165009700+atulkumarkashyap@users.noreply.github.com>
2025-08-05 10:52:38 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name = "aman"
+const repo = 50
+
+console.log(name + repo + " value")
+console.log(`hello my name is ${name} and my repo count is ${repo}`) // string interpolation
+
+  const gameName = new String("aman")
+console.log(gameName[0]) 
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('n'))
+
+
+
+
+
+
+
