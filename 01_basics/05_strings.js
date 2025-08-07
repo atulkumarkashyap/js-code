8c6b86a824b590947d084b0f4183b035b9d1a048
Atul Kumar <165009700+atulkumarkashyap@users.noreply.github.com>
2025-08-07 09:33:59 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,11 +4,28 @@ const repo = 50
 console.log(name + repo + " value")
 console.log(`hello my name is ${name} and my repo count is ${repo}`) // string interpolation
 
-  const gameName = new String("aman")
+const gameName = new String("aman")
 console.log(gameName[0]) 
 console.log(gameName.toUpperCase())
 console.log(gameName.charAt(2))
 console.log(gameName.indexOf('n'))
+
+const newString = gameName.substring(0,4)
+console.log(newString)
+
+
+
+const anotherString = gameName.slice(-4,2)
+console.log(anotherString)
+
+const newStringOne = "  aman  ";
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+
+
+
+
 
 
 
