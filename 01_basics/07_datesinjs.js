b1cda8e6054a6903ff8270bba5f4e5c919b014cb
Atul Kumar <165009700+atulkumarkashyap@users.noreply.github.com>
2025-08-07 10:43:08 +0000

    dates in js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,14 @@
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleString())
+console.log(myDate.toDateString())
+
+console.log(typeof myDate)
+
+
+
+
+
+
